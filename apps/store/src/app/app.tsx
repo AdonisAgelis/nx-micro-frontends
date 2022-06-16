e21a177ff144de8785cf6936c1a8a3c988e128db
antonis-angelis <agelisadonis@gmail.com>
2022-06-16 14:26:54 +0300

    create a shared React library in an nx workspace

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -2,6 +2,8 @@ import { getAllGames } from '../fake-api';
 
 import styles from './app.module.scss';
 
+import { Header } from '@nx-demo/store/ui-shared';
+
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
@@ -10,37 +12,44 @@ import Typography from '@material-ui/core/Typography';
 
 export const App = () => {
   return (
-    <div className={styles['container']}>
-      <div className={styles['gamesLayout']}>
-        {getAllGames().map((x) => (
-          <Card key={x.id} className={styles['gameCard']}>
-            <CardActionArea>
-              <CardMedia
-                className={styles['gameCardMedia']}
-                image={x.image}
-                title={x.name}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {x.name}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {x.description}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  component="p"
-                  className={styles['gameRating']}
-                >
-                  <strong>Rating:</strong> {x.rating}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
+    <>
+      <Header />
+      <div className={styles['container']}>
+        <div className={styles['gamesLayout']}>
+          {getAllGames().map((x) => (
+            <Card key={x.id} className={styles['gameCard']}>
+              <CardActionArea>
+                <CardMedia
+                  className={styles['gameCardMedia']}
+                  image={x.image}
+                  title={x.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {x.name}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {x.description}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                    className={styles['gameRating']}
+                  >
+                    <strong>Rating:</strong> {x.rating}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
